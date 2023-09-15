80a5e1ca07c70e30ca4fb718bbc0224f0287505f
Ramesh Nair <ram@hiddentao.com>
2023-09-15 19:14:57 +0100

    test: cleanup

--- test/deploy.foundry.test.ts
+++ test/deploy.foundry.test.ts
@@ -1,22 +1,7 @@
 import 'mocha'
-import { setTimeout } from 'node:timers/promises'
-import { ExecDaemonResult, execDaemon } from './utils.js'
-import { expect } from 'chai'
 
 describe("Command: deploy() - Foundry", () => {
-  let testNetwork: ExecDaemonResult
-
-  // before(async () => {
-  //   testNetwork = execDaemon('anvil', ['--port', 55845])
-  //   await setTimeout(2000)
-  //   expect(testNetwork.stdout).to.contain('Listening on')
-  // })
-
-  // after(() => {
-  //   testNetwork.shutdown()
-  // })
-
-  it.only('deploys the project', async () => {
+  it('deploys the project', async () => {
     console.log('TODO')
   })
 })
