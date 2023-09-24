783587adf9ca8befd5dbd0c7487e7a5bc5e4d726
Ramesh Nair <ram@hiddentao.com>
2023-09-24 13:23:41 +0100

    feat: query() command - #12

--- src/gemforge.ts
+++ src/gemforge.ts
@@ -5,6 +5,7 @@ import { command as init } from './commands/init.js'
 import { command as scaffold } from './commands/scaffold.js'
 import { command as build } from './commands/build.js'
 import { command as deploy } from './commands/deploy.js'
+import { command as query } from './commands/query.js'
 
 const cli = new Command()
 
@@ -14,6 +15,7 @@ cli
   .addCommand(scaffold())
   .addCommand(build())
   .addCommand(deploy())
+  .addCommand(query())
 
 // in NPM global mode, the first two args are the node binary and the bin .js script path, so let's make sure we grab exactly the args we want
 const realStartIndex = process.argv.findLastIndex(arg => arg.endsWith('/gemforge')) - 1
